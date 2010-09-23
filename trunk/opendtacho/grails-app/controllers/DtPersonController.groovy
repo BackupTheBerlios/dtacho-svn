040a117bc7d379a19f9a7b97f4e7962f66a9fa5b
pnaegele <pnaegele@bee3eeba-8940-0410-a8fe-d0b7de20b161>
2010-09-23 13:44:41 +0000

    Add GPLv2 header to domain and controller source files Added additional field "trafficsignalvalue" to DtPerson.groovy Added base data listing to DtPerson controller

--- trunk/opendtacho/grails-app/controllers/DtPersonController.groovy
+++ trunk/opendtacho/grails-app/controllers/DtPersonController.groovy
@@ -1,6 +1,44 @@
+// Opendtacho - a program for analysing and interpreting digital tachograph data
+// Copyright (C) 2008  Gerald Lang, ISCL - Internet Security Consulting Lang, http://www.iscl.de
+//
+// http://www.opendtacho.org
+//
+// gerald.lang@iscl.de
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import org.opendtacho.domain.DtPerson
 
 class DtPersonController {
+   // As part of the Base data update, Person should be both viewable and editable to accomodate for new data-based
+   // Authentification framework.
 
-    def scaffold = DtPerson
+    def authenticateService
+
+	// the delete, save and update actions only accept POST requests
+	static Map allowedMethods = [delete: 'POST', save: 'POST', update: 'POST']
+
+	def index = {
+		redirect action: list, params: params
+	}
+
+	def list = {
+		if (!params.max) {
+			params.max = 10
+		}
+		[personList: DtUser.list(params)]
+	}
+    // def scaffold = DtPerson
 }
