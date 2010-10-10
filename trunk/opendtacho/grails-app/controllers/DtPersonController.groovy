57db268e68062d090eaf73c099739d94aee697f7
lha <lha@bee3eeba-8940-0410-a8fe-d0b7de20b161>
2010-10-10 20:59:25 +0000

    data-based authentication

--- trunk/opendtacho/grails-app/controllers/DtPersonController.groovy
+++ trunk/opendtacho/grails-app/controllers/DtPersonController.groovy
@@ -22,6 +22,137 @@
 import org.opendtacho.domain.DtPerson
 
 class DtPersonController {
+  def authenticateService
 
   def scaffold = DtPerson
+
+  def index = { redirect(action:list,params:params) }
+
+    // the delete, save and update actions only accept POST requests
+    static allowedMethods = [delete:'POST', save:'POST', update:'POST']
+
+    def list = {
+        def currentPersonId = authenticateService.userDomain().getPerson().id
+        def currentPerson = DtPerson.get(currentPersonId)
+
+        def currentRoleMap = authenticateService.userDomain().getAuthorities().toList()
+        def currentRole = currentRoleMap[0].getAuthority()
+
+        //see everything
+        if(currentRole=="ROLE_ADMIN"){
+            return [ dtPersonInstanceList: DtPerson.list( params ), dtPersonInstanceTotal: DtPerson.count() ]
+        }
+
+        //see all persons to the company
+        if(currentRole=="ROLE_COMPANY_MANAGER"){
+            def currentCompany = currentPerson.getCompany()
+
+            def tempList = currentCompany.getPersons()
+
+            return [ dtPersonInstanceList: tempList, dtPersonInstanceTotal: tempList.count() ]
+        }
+
+        //see all persons to subsidiary
+        if(currentRole=="ROLE_SUBSIDIARY_MANAGER"){
+            def currentSubsidiary = currentPerson.getSubsidiary()
+
+            def tempList = currentSubsidiary.getPersons()
+
+            return [ dtPersonInstanceList: tempList, dtPersonInstanceTotal: tempList.count() ]
+        }
+
+        //see all persons to department
+        if(currentRole=="ROLE_DEPARTMENT_MANAGER"){
+            def currentDepartment = currentPerson.getDepartment()
+
+            def tempList = currentDepartment.getPersons()
+
+            return [ dtPersonInstanceList: tempList, dtPersonInstanceTotal: tempList.count() ]
+        }
+    }
+
+    def show = {
+        def dtPersonInstance = DtPerson.get( params.id )
+
+        if(!dtPersonInstance) {
+            flash.message = "DtPerson not found with id ${params.id}"
+            redirect(action:list)
+        }
+        else { return [ dtPersonInstance : dtPersonInstance ] }
+    }
+
+    def delete = {
+        def dtPersonInstance = DtPerson.get( params.id )
+        if(dtPersonInstance) {
+            try {
+                dtPersonInstance.delete(flush:true)
+                flash.message = "DtPerson ${params.id} deleted"
+                redirect(action:list)
+            }
+            catch(org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "DtPerson ${params.id} could not be deleted"
+                redirect(action:show,id:params.id)
+            }
+        }
+        else {
+            flash.message = "DtPerson not found with id ${params.id}"
+            redirect(action:list)
+        }
+    }
+
+    def edit = {
+        def dtPersonInstance = DtPerson.get( params.id )
+
+        if(!dtPersonInstance) {
+            flash.message = "DtPerson not found with id ${params.id}"
+            redirect(action:list)
+        }
+        else {
+            return [ dtPersonInstance : dtPersonInstance ]
+        }
+    }
+
+    def update = {
+        def dtPersonInstance = DtPerson.get( params.id )
+        if(dtPersonInstance) {
+            if(params.version) {
+                def version = params.version.toLong()
+                if(dtPersonInstance.version > version) {
+
+                    dtPersonInstance.errors.rejectValue("version", "dtPerson.optimistic.locking.failure", "Another user has updated this DtPerson while you were editing.")
+                    render(view:'edit',model:[dtPersonInstance:dtPersonInstance])
+                    return
+                }
+            }
+            dtPersonInstance.properties = params
+            if(!dtPersonInstance.hasErrors() && dtPersonInstance.save()) {
+                flash.message = "DtPerson ${params.id} updated"
+                redirect(action:show,id:dtPersonInstance.id)
+            }
+            else {
+                render(view:'edit',model:[dtPersonInstance:dtPersonInstance])
+            }
+        }
+        else {
+            flash.message = "DtPerson not found with id ${params.id}"
+            redirect(action:list)
+        }
+    }
+
+    def create = {
+        def dtPersonInstance = new DtPerson()
+        dtPersonInstance.properties = params
+        return ['dtPersonInstance':dtPersonInstance]
+    }
+
+    def save = {
+        def dtPersonInstance = new DtPerson(params)
+        if(!dtPersonInstance.hasErrors() && dtPersonInstance.save()) {
+            flash.message = "DtPerson ${dtPersonInstance.id} created"
+            redirect(action:show,id:dtPersonInstance.id)
+        }
+        else {
+            render(view:'create',model:[dtPersonInstance:dtPersonInstance])
+        }
+    }
 }
